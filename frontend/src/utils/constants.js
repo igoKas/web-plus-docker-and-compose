a5f793f674533b372178dc7dd26198997f758488
Igor Kashapov <grkasapov@gmail.com>
2024-08-19 13:26:10 +0500

    url change

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "https://api.kpdigkas.nomoredomainswork.ru";
+export const URL = "https://api.kpd.igokas.ru";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
